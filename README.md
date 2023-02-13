efc7f62b8992488647a88bb3fed436dd99d1fc38
erfan gharib <erfangharib5@gmail.com>
2023-02-13 18:20:03 +0330

    user can convert Miladi month to Shamsi month & vice versa

---
+++ README.md
@@ -0,0 +1,7 @@
+# Show Shamsi Time
+
+## Installation
+run <code>npm install -g</code> to make command global
+
+## Run
+run using <code>stime</code> command
--- index.js
+++ index.js
@@ -1,7 +1,58 @@
 #! node
 
-const shamsi = require('shamsi-date-converter');
-const date = new Date();
-const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+const shamsi       = require('shamsi-date-converter'),
+      shamsiMonths = require('./data/shamsi.json'),
+      miladiMonths = require('./data/miladi.json');
 
-console.log(`\n    Date: ${shamsi.gregorianToJalali(date).join('/')}\n    Time: ${time}\n`);
+const date = new Date(),
+      time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`,
+      [filePath, exePath, argv2, argv3] = process.argv;
+
+switch (argv2) {
+    case '-m': {
+        if (Number(argv3) <= 12) {
+            console.log(
+                `${argv3}th Month of:`,
+                `\n- Shamsi: ${shamsiMonths[argv3].month}`,
+                `\n- Miladi: ${miladiMonths[argv3].month}, ${miladiMonths[argv3].month.substring(0, 3)}s`
+            );
+        }
+        else {
+            try {
+                console.log(
+                    shamsiMonths.filter(v => {
+                        return v.month.toLowerCase() === argv3.toLowerCase();
+                    })[0].eq
+                )
+            } catch {
+                console.log(
+                    miladiMonths.filter(v => {
+                        return (
+                            v.month.toLowerCase() === argv3.toLowerCase() ||
+                            v.month.toLowerCase().substring(0,3) === argv3.toLowerCase()
+                        )
+                    })[0].eq
+                )
+            }
+        }
+        break;
+    }
+
+    case '-d':
+
+        break;
+    case '-y':
+
+        break;
+    case '-h': case ' ':
+
+        break;
+
+    default:
+        console.log(
+            `Date: ${shamsi.gregorianToJalali(date).join('/')}`,
+            `\nTime: ${time}`
+        );
+        break;
+}
+// } 
